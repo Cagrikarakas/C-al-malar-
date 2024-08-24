0e175dcfbee94a590d33185133a5f162be684da7
Çağrı <109808428+Cagrikarakas@users.noreply.github.com>
2024-08-24 18:02:58 +0300

    Add files via upload

---
+++ 1.Ders - Ekrana yazı yazdırma - ve yorum satırı/1.Ders - Ekrana yazı yazdırma - ve yorum satırı/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Ders___Ekrana_yazı_yazdırma___ve_yorum_satırı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");   
+            Console.WriteLine("2024 15 kasımda ilk arabamı alacağım !")
+            Console.Write("bu ŞEKİLDE yazınca");
+            Console.Write("YAN yana Yazıyor");
+            Console.ReadLine();    
+        }
+    }
+}
